7b660fac26f8b3dcc8cfcb0c4671a924e0aa884b
Joseph Parker <jparker010190@gmail.com>
2018-09-23 18:47:16 +0000

    automatically backed up by learn

--- src/components/PoemWrites.js
+++ src/components/PoemWrites.js
@@ -21,27 +21,27 @@ class PoemWriter extends React.Component {
     if (poem.length === 0) return false;
     const lines = poem.split("\n");
     const wordCount = lines.map((l) => l.trim().split(' ').length)
- 
-    return (lines.length === 3 && 
-        wordCount[0] === 5 && 
+
+    return (lines.length === 3 &&
+        wordCount[0] === 5 &&
         wordCount[1] === 3 &&
-        wordCount[2] === 5) ? 
-        true : 
+        wordCount[2] === 5) ?
+        true :
         false
   }
 
   render() {
     return (
       <div>
-        <textarea 
+        <textarea
           value={this.state.poem}
           onChange={this.handleChange}
-          rows="3" 
-          cols="60" 
+          rows="3"
+          cols="60"
         />
         {!this.state.isValid &&
-          <div 
-            id="poem-validation-error" 
+          <div
+            id="poem-validation-error"
             style={{color: 'red'}}
           >
             This poem is not written in the right structure!
